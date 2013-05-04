45214332f620c7b263985269071573a462ae3ae1
chanwookpark <chanwook.god@gmail.com>
2013-05-05 01:44:02 +0900

    resolve to basic Dust view's class and flow (compile->load->render)

---
+++ src/main/java/framewise/dustview/DustLoader.java
@@ -0,0 +1,153 @@
+package framewise.dustview;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.Scriptable;
+
+/**
+ * 
+ * @author chanwook
+ * 
+ */
+public class DustLoader {
+
+	public static final String DEFAULT_DUST_JS_FILE_PATH = "/dust/dust-full-1.1.1.js";
+	public static final String DEFAULT_DUST_HELPER_JS_FILE_PATH = "/dust/dust-helpers-1.1.0.js";
+
+	public static final String DEFAULT_COMPILE_SCRIPT = "(dust.compile(source, templateKey))";
+	public static final String DEFAULT_LOAD_SCRIPT = "(dust.loadSource(compiledSource))";
+	public static final String DEFAULT_RENDER_SCRIPT = ("{   dust.render( templateKey, JSON.parse(json), "
+			+ "function(error, data) { if(error) { writer.write(error);} else { writer.write( data );} } );}");
+
+	private String dustJsFilePath = DEFAULT_DUST_JS_FILE_PATH;
+	private String dustJsHelperFilePath = DEFAULT_DUST_HELPER_JS_FILE_PATH;
+
+	private String compileScript = DEFAULT_COMPILE_SCRIPT;
+	private String loadScript = DEFAULT_LOAD_SCRIPT;
+	private String renderScript = DEFAULT_RENDER_SCRIPT;
+
+	public static Scriptable globalScope;
+
+	public DustLoader() {
+	}
+
+	public void initializeContext() {
+		InputStream dustJsStream = getDustJsStream(getDustJsFilePath());
+		InputStream dustHelperJsStream = getDustJsStream(getDustJsHelperFilePath());
+
+		loadDustJsEngine(dustJsStream, dustHelperJsStream);
+	}
+
+	/**
+	 * Initialize Dust JS Context
+	 * 
+	 * @param dustJsStream
+	 * @param dustHelperJsStream
+	 */
+	protected void loadDustJsEngine(InputStream dustJsStream, InputStream dustHelperJsStream) {
+		try {
+			Reader dustJsReader = new InputStreamReader(dustJsStream, "UTF-8");
+			Reader dustJsHelperReader = new InputStreamReader(dustHelperJsStream, "UTF-8");
+
+			Context context = Context.enter();
+			context.setOptimizationLevel(9);
+
+			globalScope = context.initStandardObjects();
+			context.evaluateReader(globalScope, dustJsReader, "dust-full-1.1.1.js", dustJsStream.available(), null);
+			context.evaluateReader(globalScope, dustJsHelperReader, "dust-helpers-1.1.0.js",
+					dustHelperJsStream.available(), null);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			Context.exit();
+		}
+	}
+
+	public String compile(String source, String templateKey) {
+		Context context = Context.enter();
+
+		Scriptable compileScope = context.newObject(globalScope);
+		compileScope.setParentScope(globalScope);
+
+		compileScope.put("source", compileScope, source);
+		compileScope.put("templateKey", compileScope, templateKey);
+
+		try {
+			return (String) context.evaluateString(compileScope, compileScript, "JDustCompiler", 0, null);
+		} catch (JavaScriptException e) {
+			throw new RuntimeException("thrown error when compile Dust JS Source", e);
+		}
+	}
+
+	/**
+	 * @param compiledSource
+	 */
+	public void load(String compiledSource) {
+		Context context = Context.enter();
+
+		Scriptable compileScope = context.newObject(globalScope);
+		compileScope.setParentScope(globalScope);
+
+		compileScope.put("compiledSource", compileScope, compiledSource);
+
+		try {
+			context.evaluateString(compileScope, loadScript, "JDustCompiler", 0, null);
+		} catch (JavaScriptException e) {
+			throw new RuntimeException("thrown error when load Dust JS Source", e);
+		}
+	}
+
+	/**
+	 * @param writer
+	 * @param templateKey
+	 * @param json
+	 */
+	protected void render(Writer writer, String templateKey, String json) {
+		Context context = Context.enter();
+
+		Scriptable renderScope = context.newObject(globalScope);
+		renderScope.setParentScope(globalScope);
+
+		try {
+			renderScope.put("writer", renderScope, writer);
+			renderScope.put("json", renderScope, json);
+			renderScope.put("templateKey", renderScope, templateKey);
+
+			context.evaluateString(renderScope, renderScript, "JDustCompiler", 0, null);
+
+		} catch (JavaScriptException e) {
+			throw new RuntimeException("thrown error when Rendering Dust JS Source", e);
+		}
+	}
+
+	/**
+	 * Resolve File InputStream by Path
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	public InputStream getDustJsStream(String filePath) {
+		InputStream resourceStream = getClass().getResourceAsStream(filePath);
+		return resourceStream;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getDustJsFilePath() {
+		return dustJsFilePath;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getDustJsHelperFilePath() {
+		return dustJsHelperFilePath;
+	}
+}
---
+++ src/test/java/framewise/dustview/DustLoadTests.java
@@ -0,0 +1,81 @@
+package framewise.dustview;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.io.StringWriter;
+
+import org.junit.Test;
+import org.mozilla.javascript.Context;
+
+public class DustLoadTests {
+
+	@Test
+	public void findDustJsFile() throws Exception {
+		DustLoader loader = new DustLoader();
+		// Load Dust Js file
+		InputStream dustJsFileStream = loader.getDustJsStream(loader.getDustJsFilePath());
+		assertThat(dustJsFileStream, notNullValue());
+		assertThat(dustJsFileStream.available(), is(102697));
+
+		// Load Dust Helper Js file
+		InputStream dustJsHelperFileStream = loader.getDustJsStream(loader.getDustJsHelperFilePath());
+		assertThat(dustJsHelperFileStream, notNullValue());
+		assertThat(dustJsHelperFileStream.available(), is(18324));
+	}
+
+	@Test
+	public void loadJsContextToDustJs() throws Exception {
+		DustLoader loader = new DustLoader();
+		loader.initializeContext();
+	}
+
+	@Test
+	public void compile2load2renderinDustJsSource() throws Exception {
+		DustLoader loader = new DustLoader();
+		loader.initializeContext();
+		String source = "Hello World!";
+		// compile
+		String compiled = loader.compile(source, "test1");
+		assertThat(
+				compiled,
+				is("(function(){dust.register(\"test1\",body_0);function body_0(chk,ctx){return chk.write(\"Hello World!\");}return body_0;})();"));
+
+		// load
+		loader.load(compiled);
+
+		// render
+		StringWriter writer = new StringWriter();
+		loader.render(writer, "test1", "{}");
+		assertThat(writer.getBuffer().toString(), is("Hello World!"));
+	}
+
+	@Test
+	public void renderWithJson() throws Exception {
+		DustLoader loader = new DustLoader();
+		loader.initializeContext();
+		String source = "Hello {name} World!";
+		// compile
+		String compiled = loader.compile(source, "test2");
+		assertThat(
+				compiled,
+				is("(function(){dust.register(\"test2\",body_0);function body_0(chk,ctx){return chk.write(\"Hello \").reference(ctx.get(\"name\"),ctx,\"h\").write(\" World!\");}return body_0;})();"));
+
+		// load
+		loader.load(compiled);
+
+		// render
+		StringWriter writer = new StringWriter();
+		loader.render(writer, "test2", "{\"name\":\"chanwook\"}");
+		assertThat(writer.getBuffer().toString(), is("Hello chanwook World!"));
+	}
+
+	static class TestDustLoader extends DustLoader {
+		@Override
+		protected void loadDustJsEngine(InputStream dustJsStream, InputStream dustHelperJsStream) {
+			super.loadDustJsEngine(dustJsStream, dustHelperJsStream);
+		}
+	}
+}
